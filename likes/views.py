31ab51b4260dcbb637d97d21882e20c708fd4e95
Harry Hayden <h_man1992@hotmail.com>
2023-01-24 16:26:17 +0000

    add followers

--- likes/views.py
+++ likes/views.py
@@ -1,4 +1,4 @@
-from rest_framework import generics, views
+from rest_framework import generics, permissions
 from drf_api.permissions import IsOwnerOrReadOnly
 from likes.models import Like
 from likes.serializers import LikeSerializer
