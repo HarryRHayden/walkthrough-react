d5bc888014ee18d04de3ba1362318224428ce4aa
Harry Hayden <h_man1992@hotmail.com>
2023-01-27 14:55:19 +0000

    update data

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -140,17 +140,11 @@ WSGI_APPLICATION = 'drf_api.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-if os.environ.get("DATABASE_URL"):
-    DATABASES = {
+
+DATABASES = {
         "default": dj_database_url.parse(os.environ.get("DATABASE_URL"))
     }
-else:
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.sqlite3",
-            "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
-        }
-    }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
